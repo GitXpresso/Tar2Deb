add598a7ddd1c7aea62654f34ba5082f1f502ef6
William <126926699+GitXpresso@users.noreply.github.com>
2025-03-14 15:47:27 +0000

    Update installtar2deb.sh

--- installtar2deb.sh
+++ installtar2deb.sh
@@ -1,1 +1,1 @@
-sudo apt install wget && wget https://github.com/GitXpresso/Tar2Deb/releases/download/v.1.0.0/tar2deb-1.0.0.deb && sudo apt install -y ./tar2deb-1.0.0.deb
+sudo apt install wget && wget https://github.com/GitXpresso/Tar2Deb/releases/download/v.1.0.0/tar2deb-1.0.0.deb && sudo apt install ./tar2deb-1.0.0.deb && sudo rm -rf tar2deb-1.0.0.deb
