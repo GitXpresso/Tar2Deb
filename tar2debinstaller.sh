459f8fc9efed7313c37df53e5b69185e7adcad71
William <126926699+GitXpresso@users.noreply.github.com>
2025-03-20 16:47:22 +0000

    Update tar2debinstaller.sh
    Added if you want to remove the file or no prompt

--- tar2debinstaller.sh
+++ tar2debinstaller.sh
@@ -1,11 +1,52 @@
+functions:
+v1.0.0(){
+read -p "Do you want remove "tar2deb-1.0.0.deb"? (yes/no): " yesorno
+if [ "$yesorno" = "yes" ]; then
+rm -rf ~/tar2deb-1.0.0.deb
+elif [ "$yesorno" = "no" ]; then 
+echo "not removing tar2deb-1.0.0.deb"
+exit 1
+else
+echo "invalid answer"
+exit 1
+fi
+}
+v1.0.1(){
+read -p "Do you want remove "tar2deb-1.0.1.deb"? (yes/no): " yesorno
+if [ "$yesorno" = "yes" ]; then
+rm -rf ~/tar2deb-1.0.1.deb
+elif [ "$yesorno" = "no" ]; then 
+echo "not removing tar2deb-1.0.1.deb"
+exit 1
+else
+echo "invalid answer"
+exit 1
+fi
+}
+v1.0.2(){
+read -p "Do you want remove "tar2deb-1.0.2.deb"? (yes/no): " yesorno
+if [ "$yesorno" = "yes" ]; then
+rm -rf ~/tar2deb-1.0.2.deb
+elif [ "$yesorno" = "no" ]; then 
+echo "not removing tar2deb-1.0.2.deb"
+exit 1
+else
+echo "invalid answer"
+exit 1
+fi
+}
 echo "avaliable tag versions: "
-echo "1. 1.0.0 (first release)"
-echo "2. 1.0.1 (second release)"
+echo "1. 1.0.0 "
+echo "2. 1.0.1 "
+echo "2. 1.0.2  "
 read -p "enter a number containing a tag version (ex: 1.0.0): " tag
 if [ "$tag" = "1" ]; then
 wget -P ~/ https://github.com/GitXpresso/Tar2Deb/releases/download/v1.0.0/tar2deb-1.0.1.deb && sudo apt install ~/tar2deb-1.0.0.deb
+v1.0.0
 elif [ "$tag" = "2" ]; then 
 wget -P ~/ https://github.com/GitXpresso/Tar2Deb/releases/download/v1.0.1/tar2deb-1.0.1.deb && sudo apt install ~/tar2deb-1.0.1.deb
+v1.0.1
+elif [
 else
 echo "invalid input, exiting..."
 fi
