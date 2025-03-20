aa97217a428841bc722b2922458fd503eb1186c2
William <126926699+GitXpresso@users.noreply.github.com>
2025-03-20 16:55:42 +0000

    Update tar2debinstaller.sh

--- tar2debinstaller.sh
+++ tar2debinstaller.sh
@@ -1,79 +1,55 @@
-# Define the functions
-v1.0.0() {
-  read -p "Do you want to remove 'tar2deb-1.0.0.deb'? (yes/no): " yesorno
-  if [ "$yesorno" = "yes" ]; then
-    rm -rf ~/tar2deb-1.0.0.deb
-  elif [ "$yesorno" = "no" ]; then
-    echo "not removing tar2deb-1.0.0.deb"
-  else
-    echo "invalid answer"
-    exit 1
-  fi
+# functions:
+v1.0.0(){
+read -p "Do you want remove "tar2deb-1.0.0.deb"? (yes/no): " yesorno
+if [ "$yesorno" = "yes" ]; then
+rm -rf ~/tar2deb-1.0.0.deb
+elif [ "$yesorno" = "no" ]; then 
+echo "not removing tar2deb-1.0.0.deb"
+exit 1
+else
+echo "invalid answer"
+exit 1
+fi
 }
-
-v1.0.1() {
-  read -p "Do you want to remove 'tar2deb-1.0.1.deb'? (yes/no): " yesorno
-  if [ "$yesorno" = "yes" ]; then
-    rm -rf ~/tar2deb-1.0.1.deb
-  elif [ "$yesorno" = "no" ]; then
-    echo "not removing tar2deb-1.0.1.deb"
-  else
-    echo "invalid answer"
-    exit 1
-  fi
+v1.0.1(){
+read -p "Do you want remove "tar2deb-1.0.1.deb"? (yes/no): " yesorno
+if [ "$yesorno" = "yes" ]; then
+rm -rf ~/tar2deb-1.0.1.deb
+elif [ "$yesorno" = "no" ]; then 
+echo "not removing tar2deb-1.0.1.deb"
+exit 1
+else
+echo "invalid answer"
+exit 1
+fi
 }
-
-v1.0.2() {
-  read -p "Do you want to remove 'tar2deb-1.0.2.deb'? (yes/no): " yesorno
-  if [ "$yesorno" = "yes" ]; then
-    rm -rf ~/tar2deb-1.0.2.deb
-  elif [ "$yesorno" = "no" ]; then
-    echo "not removing tar2deb-1.0.2.deb"
-  else
-    echo "invalid answer"
-    exit 1
-  fi
+v1.0.2(){
+read -p "Do you want remove "tar2deb-1.0.2.deb"? (yes/no): " yesorno
+if [ "$yesorno" = "yes" ]; then
+rm -rf ~/tar2deb-1.0.2.deb
+elif [ "$yesorno" = "no" ]; then 
+echo "not removing tar2deb-1.0.2.deb"
+exit 1
+else
+echo "invalid answer"
+exit 1
+fi
 }
-
-tryagain() {
-  echo "available tag versions: "
-  echo "1. 1.0.0 "
-  echo "2. 1.0.1 "
-  echo "3. 1.0.2"
-  read -p "Enter a number containing a tag version (ex: 1.0.0): " tag
-
-  if [ "$tag" = "1" ]; then
-    wget -P ~/ https://github.com/GitXpresso/Tar2Deb/releases/download/v1.0.0/tar2deb-1.0.0.deb && sudo apt install ~/tar2deb-1.0.0.deb
-    v1.0.0
-  elif [ "$tag" = "2" ]; then
-    wget -P ~/ https://github.com/GitXpresso/Tar2Deb/releases/download/v1.0.1/tar2deb-1.0.1.deb && sudo apt install ~/tar2deb-1.0.1.deb
-    v1.0.1
-  elif [ "$tag" = "3" ]; then
-    wget -P ~/ https://github.com/GitXpresso/Tar2Deb/releases/download/v1.0.2/tar2deb-1.0.2.deb && sudo apt install ~/tar2deb-1.0.2.deb
-    v1.0.2
-  else
-    echo "invalid input, try again"
-    tryagain
-  fi
-}
-
-# Main part of the script
-echo "available tag versions: "
+echo "avaliable tag versions: "
 echo "1. 1.0.0 "
 echo "2. 1.0.1 "
-echo "3. 1.0.2"
-read -p "Enter a number containing a tag version (ex: 1.0.0): " tag
-
+echo "2. 1.0.2  "
+read -p "enter a number containing a tag version (ex: 1.0.0): " tag
 if [ "$tag" = "1" ]; then
-  wget -P ~/ https://github.com/GitXpresso/Tar2Deb/releases/download/v1.0.0/tar2deb-1.0.0.deb && sudo apt install ~/tar2deb-1.0.0.deb
-  v1.0.0
-elif [ "$tag" = "2" ]; then
-  wget -P ~/ https://github.com/GitXpresso/Tar2Deb/releases/download/v1.0.1/tar2deb-1.0.1.deb && sudo apt install ~/tar2deb-1.0.1.deb
-  v1.0.1
+wget -P ~/ https://github.com/GitXpresso/Tar2Deb/releases/download/v1.0.0/tar2deb-1.0.1.deb && sudo apt install ~/tar2deb-1.0.0.deb
+v1.0.0
+elif [ "$tag" = "2" ]; then 
+wget -P ~/ https://github.com/GitXpresso/Tar2Deb/releases/download/v1.0.1/tar2deb-1.0.1.deb && sudo apt install ~/tar2deb-1.0.1.deb
+v1.0.1
 elif [ "$tag" = "3" ]; then
-  wget -P ~/ https://github.com/GitXpresso/Tar2Deb/releases/download/v1.0.2/tar2deb-1.0.2.deb && sudo apt install ~/tar2deb-1.0.2.deb
-  v1.0.2
+wget -P ~/ https://github.com/GitXpresso/Tar2Deb/releases/download/v1.0.2/tar2deb-1.0.2.deb && sudo apt install ~/tar2deb-1.0.2.deb
+v1.0.2
 else
-  echo "invalid input, try again"
-  tryagain
+echo "invalid input, exiting..."
+exit 1
 fi
