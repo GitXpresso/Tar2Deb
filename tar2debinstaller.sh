45431ed38699f8293f86956c8edec5b1a6ef2546
William <126926699+GitXpresso@users.noreply.github.com>
2025-03-20 16:50:56 +0000

    Update tar2debinstaller.sh

--- tar2debinstaller.sh
+++ tar2debinstaller.sh
@@ -42,7 +42,7 @@ echo "2. 1.0.1 "
 echo "2. 1.0.2  "
 read -p "enter a number containing a tag version (ex: 1.0.0): " tag
 if [ "$tag" = "1" ]; then
-wget -P ~/ https://github.com/GitXpresso/Tar2Deb/releases/download/v1.0.0/tar2deb-1.0.1.deb && sudo apt install ~/tar2deb-1.0.0.deb
+wget -P ~/ https://github.com/GitXpresso/Tar2Deb/releases/download/v1.0.0/tar2deb-1.0.0.deb && sudo apt install ~/tar2deb-1.0.0.deb
 v1.0.0
 elif [ "$tag" = "2" ]; then 
 wget -P ~/ https://github.com/GitXpresso/Tar2Deb/releases/download/v1.0.1/tar2deb-1.0.1.deb && sudo apt install ~/tar2deb-1.0.1.deb
